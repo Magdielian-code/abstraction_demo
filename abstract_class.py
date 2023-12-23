acc9639727d17e694b5fc757743ade07f0ce2352
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-12-23 18:49:53 +0100

    created the abstract_classes file

---
+++ abstract_class.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    def __init__(self,n):
+        self.no_of_tyres = n
+    
+    @abstractmethod
+    def start(self):
+        pass
+    def display(self):
+        print("Hi I am calling fro vehicle class.")
+
